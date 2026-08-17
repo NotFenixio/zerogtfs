739baa6ea469db3bcb754b553c7e89f77aa1723d
Fen <103071021+NotFenixio@users.noreply.github.com>
2026-08-17 14:49:10 +0200

    feat: Implement FFI function to encode GTFS data from source

---
+++ ffi_bridge.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"fmt"
+)
+
+//export GenerateZeroGTFS
+func GenerateZeroGTFS(cSource *C.char, cOutput *C.char) C.int {
+	source := C.GoString(cSource)
+	outputFile := C.GoString(cOutput)
+
+	var data *MockGTFSData
+	var err error
+
+	if source != "" && source != "mock" {
+		data, err = loadGTFSData(source)
+		if err != nil {
+			fmt.Printf("[Go FFI Error] Failed loading GTFS: %v\n", err)
+			return 0
+		}
+	} else {
+		data = NewMockData()
+	}
+
+	encoder := NewZeroGTFSEncoder(data)
+	if err := encoder.Encode(outputFile); err != nil {
+		fmt.Printf("[Go FFI Error] Failed encoding binary format: %v\n", err)
+		return 0
+	}
+
+	return 1 // success!
+}
+
+// required to compile in shared c mode thing
+func main() {}
